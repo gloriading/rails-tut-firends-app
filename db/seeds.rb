1aaaaee856bd683fa62d7b8b51994a087ec4f665
Gloria Ding <gloria.hc.ding@gmail.com>
2021-12-27 14:31:38 -0800

    Add hirb gem to easy reading console output
    Add faker for db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+    Friend.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: Faker::Internet.safe_email,
+        phone: Faker::PhoneNumber.cell_phone,
+        address: Faker::Address.full_address,
+        user_id: 2,
+    )
+end
