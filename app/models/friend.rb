f51dd272f1ec1c05b6153d8d2f54ca3438d6c5fd
Gloria Ding <gloria.hc.ding@gmail.com>
2021-12-27 17:02:16 -0800

    Add validations, callbacks, and scope to firend model

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,20 @@
 class Friend < ApplicationRecord
     belongs_to :user
+    validates(:email, :first_name, :last_name, presence: { message: "must be given" }, uniqueness: true )
+    validate :no_asdf
+    before_save :title_names
+    scope :recent_three, -> { order("created_at DESC").limit(3) }
+
+    private
+
+    def no_asdf
+        if first_name.present? && first_name.downcase.include?('asdf')
+            errors.add(:first_name, "should not contain asdf!")
+        end
+    end
+
+    def title_names
+        self.first_name = self.first_name.titleize
+        self.last_name = self.last_name.titleize
+    end
 end
